3019802ebd877c6ffdb9424254ec3db08d2e625b
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-02-19 14:58:50 +0000

    home page

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return <h1>Home</h1>
+  return <>
+    <h1>Title</h1>
+    <p>Subtitle</p>
+    <div>
+      <Link
+        to={{
+          pathname: '/register'
+        }}>
+        <button>Register</button></Link>
+      <Link
+        to={{
+          pathname: '/login'
+        }}><button>Login</button></Link>
+    </div>
+  </>
 }
- 
+
 export default Home
