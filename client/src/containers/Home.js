00d378143e4a9a224b60f785f455c1dcd3d0061f
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-02-20 21:35:56 +0000

    buttons on home updated

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -2,20 +2,31 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
 
+import { getLoggedInUserId } from '../lib/auth.js'
+
 const Home = () => {
+
+  const loggedIn = getLoggedInUserId()
+
   return <>
     <h1>Title</h1>
     <p>Subtitle</p>
     <div>
-      <Link
+      {!loggedIn && <>
+        <Link
+          to={{
+            pathname: '/register'
+          }}>
+          <Button variant="primary">Register</Button></Link>
+        <Link
+          to={{
+            pathname: '/login'
+          }}><Button variant="warning">Login</Button></Link>
+      </>}
+      {loggedIn && <Link
         to={{
-          pathname: '/register'
-        }}>
-        <Button variant="primary">Register</Button></Link>
-      <Link
-        to={{
-          pathname: '/login'
-        }}><Button variant="warning">Login</Button></Link>
+          pathname: '/user'
+        }}><Button variant="warning">Profile</Button></Link>}
     </div>
   </>
 }
