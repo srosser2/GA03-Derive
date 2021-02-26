e0f33a1c2cfec137ff90558e7099d425be8fbfe1
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-02-26 11:50:49 +0000

    changes

--- config/environment.js
+++ config/environment.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const environment = process.env.NODE_ENV || 'production'
+const environment = process.env.NODE_ENV || 'development'
 
 export const dbURI = environment === 'production' ? 
   process.env.MONGODB_URI : 
   `mongodb://localhost/travelapp-${environment}`
 
 export const port = process.env.PORT || 8000
-export const secret = process.env.SECRET
+export const secret = process.env.SECRET || 'somethingdifficulttoguess'
